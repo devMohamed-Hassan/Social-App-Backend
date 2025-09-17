3d991badd30f538f39296d8c4582150b1cfc9a7e
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-09-17 17:05:26 +0300

    init: setup Express server

---
+++ src/bootstrap.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve("./src/config/.env"),
+});
+
+const app = express();
+
+export const bootstrap = () => {
+  app.use(express.json());
+
+  const port = process.env.PORT || 5000;
+
+  app.get("/", (req, res, next) => {
+    res.json({
+      message: "Welcome to the Social App",
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+};
